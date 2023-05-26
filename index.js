66a559735ffdf8ea2ae62ddadd21e8acd858376a
BrentVanwaeyenberge <Brent.Vanwaeyenberge@student.ehb.be>
2023-05-26 13:17:29 +0200

    Mongo Setup +CRUD Requests

---
+++ index.js
@@ -0,0 +1,379 @@
+// constants
+
+const {
+    MongoClient
+} = require('mongodb');
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const port = 3000
+const uri = "mongodb+srv://brent:mongodeeznutz@cluster0.4su5r5h.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+const database = client.db("BrentSchool");
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+// parse application/json
+app.use(bodyParser.json())
+
+// main
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+// utility
+
+
+
+// routes
+
+app.get('/users', async (req, res) => {
+    let users = await _getUsers();
+    res.status(200).send(users);
+})
+//GET//
+app.get('/releases', async (req, res) => {
+    try {
+        // Retrieve all releases from the database
+        let releases = await Releases.find();
+        res.status(200).json(releases);
+    } catch (error) {
+        res.status(500).json({
+            error: 'Internal server error'
+        });
+    }
+});
+
+app.get('/votes', async (req, res) => {
+    try {
+        // Retrieve all votes from the database
+        let votes = await Votes.find();
+        res.status(200).json(votes);
+    } catch (error) {
+        res.status(500).json({
+            error: 'Internal server error'
+        });
+    }
+});
+//POST//
+app.post('/users', async (req, res) => {
+    try {
+        await client.connect();
+
+        let usersColl = database.collection("Users");
+        let user = {
+            "password": req.body.password,
+            "displayname": req.body.displayname,
+            "email": req.body.email
+        };
+
+        const insertedUser = await usersColl.insertOne(user);
+
+        res.status(201).send({
+            status: "Saved",
+            message: "User has been saved!",
+            data: insertedUser
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            error: 'Something went wrong!',
+            value: error
+        });
+    } finally {
+        await client.close();
+    }
+})
+
+app.post('/votes', async (req, res) => {
+    try {
+        await client.connect();
+
+        const votesColl = database.collection("Votes");
+        const vote = {
+            "userId": req.body.userId,
+            "releaseId": req.body.releaseId,
+            "value": req.body.value
+        };
+
+        const insertedVote = await votesColl.insertOne(vote);
+
+        res.status(201).send({
+            status: "Saved",
+            message: "Vote has been saved!",
+            data: insertedVote
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            error: 'Something went wrong!',
+            value: error
+        });
+    } finally {
+        await client.close();
+    }
+});
+
+app.post('/releases', async (req, res) => {
+    try {
+        await client.connect();
+
+        const releasesColl = database.collection("Releases");
+        const release = {
+            "name": req.body.name,
+            "description": req.body.description,
+            "date": req.body.date
+        };
+
+        const insertedRelease = await releasesColl.insertOne(release);
+
+        res.status(201).send({
+            status: "Saved",
+            message: "Release has been saved!",
+            data: insertedRelease
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            error: 'Something went wrong!',
+            value: error
+        });
+    } finally {
+        await client.close();
+    }
+});
+//DELETE//
+app.delete('/users/:id', async (req, res) => {
+    try {
+        await client.connect();
+
+        const usersColl = database.collection("Users");
+        const userId = req.params.id;
+
+        const deletedUser = await usersColl.findOneAndDelete({
+            _id: ObjectId(userId)
+        });
+
+        if (deletedUser.value === null) {
+            res.status(404).json({
+                error: 'User not found'
+            });
+        } else {
+            res.status(200).json({
+                status: "Deleted",
+                message: "User has been deleted",
+                data: deletedUser.value
+            });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            error: 'Internal server error'
+        });
+    } finally {
+        await client.close();
+    }
+});
+
+app.delete('/votes/:id', async (req, res) => {
+    try {
+        await client.connect();
+
+        const votesColl = database.collection("Votes");
+        const voteId = req.params.id;
+
+        const deletedVote = await votesColl.findOneAndDelete({
+            _id: ObjectId(voteId)
+        });
+
+        if (deletedVote.value === null) {
+            res.status(404).json({
+                error: 'Vote not found'
+            });
+        } else {
+            res.status(200).json({
+                status: "Deleted",
+                message: "Vote has been deleted",
+                data: deletedVote.value
+            });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            error: 'Internal server error'
+        });
+    } finally {
+        await client.close();
+    }
+});
+
+app.delete('/releases/:id', async (req, res) => {
+    try {
+        await client.connect();
+
+        const releasesColl = database.collection("Releases");
+        const releaseId = req.params.id;
+
+        const deletedRelease = await releasesColl.findOneAndDelete({
+            _id: ObjectId(releaseId)
+        });
+
+        if (deletedRelease.value === null) {
+            res.status(404).json({
+                error: 'Release not found'
+            });
+        } else {
+            res.status(200).json({
+                status: "Deleted",
+                message: "Release has been deleted",
+                data: deletedRelease.value
+            });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            error: 'Internal server error'
+        });
+    } finally {
+        await client.close();
+    }
+});
+//PUT//
+app.put('/users/:id', async (req, res) => {
+    try {
+        await client.connect();
+
+        const usersColl = database.collection("Users");
+        const userId = req.params.id;
+
+        const updatedUser = {
+            $set: {
+                "password": req.body.password,
+                "displayname": req.body.displayname,
+                "email": req.body.email
+            }
+        };
+
+        const result = await usersColl.updateOne({
+            _id: ObjectId(userId)
+        }, updatedUser);
+
+        if (result.modifiedCount === 0) {
+            res.status(404).json({
+                error: 'User not found'
+            });
+        } else {
+            res.status(200).json({
+                status: "Updated",
+                message: "User has been updated"
+            });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            error: 'Internal server error'
+        });
+    } finally {
+        await client.close();
+    }
+});
+
+app.put('/votes/:id', async (req, res) => {
+    try {
+        await client.connect();
+
+        const votesColl = database.collection("Votes");
+        const voteId = req.params.id;
+
+        const updatedVote = {
+            $set: {
+                "userId": req.body.userId,
+                "releaseId": req.body.releaseId,
+                "value": req.body.value
+            }
+        };
+
+        const result = await votesColl.updateOne({
+            _id: ObjectId(voteId)
+        }, updatedVote);
+
+        if (result.modifiedCount === 0) {
+            res.status(404).json({
+                error: 'Vote not found'
+            });
+        } else {
+            res.status(200).json({
+                status: "Updated",
+                message: "Vote has been updated"
+            });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            error: 'Internal server error'
+        });
+    } finally {
+        await client.close();
+    }
+});
+
+app.put('/releases/:id', async (req, res) => {
+    try {
+        await client.connect();
+
+        const releasesColl = database.collection("Releases");
+        const releaseId = req.params.id;
+
+        const updatedRelease = {
+            $set: {
+                "name": req.body.name,
+                "description": req.body.description,
+                "date": req.body.date
+            }
+        };
+
+        const result = await releasesColl.updateOne({
+            _id: ObjectId(releaseId)
+        }, updatedRelease);
+
+        if (result.modifiedCount === 0) {
+            res.status(404).json({
+                error: 'Release not found'
+            });
+        } else {
+            res.status(200).json({
+                status: "Updated",
+                message: "Release has been updated"
+            });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            error: 'Internal server error'
+        });
+    } finally {
+        await client.close();
+    }
+});
+
+// getters
+
+async function _getUsers() {
+    try {
+        await client.connect();
+        let usersColl = database.collection("Users");
+        let users = usersColl.find();
+        return await users.toArray();
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            error: 'Something went wrong!',
+            value: error
+        });
+    } finally {
+        await client.close();
+    }
+}
+//TEST//
